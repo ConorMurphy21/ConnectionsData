cd396a8523397b531a70d014e4e1e0dfc440c3a9
conor <conormurphyx@gmail.com>
2024-03-25 00:46:12 -0700

    * removed quotes from git save

--- src/gitUtils.py
+++ src/gitUtils.py
@@ -6,6 +6,6 @@ def save_to_git(args, user_config, file):
         return
     subprocess.call(['git', 'add', file])
     if file.is_relative_to('games'):
-        subprocess.call(['git', 'commit', '-m', f'"{user_config.username} generated new game"'])
+        subprocess.call(['git', 'commit', '-m', f'{user_config.username} generated new game'])
     else:
-        subprocess.call(['git', 'commit', '-m', f'"{user_config.username} played a game"'])
+        subprocess.call(['git', 'commit', '-m', f'{user_config.username} played a game'])
