eb7b520abd4862886f3d6b60163e66525d8d1131
conor <conormurphyx@gmail.com>
2024-04-07 14:37:07 -0700

    * fix git rebase before push

--- src/gitUtils.py
+++ src/gitUtils.py
@@ -8,11 +8,11 @@ def pull_master():
 def save_to_git(args, user_config, file):
     if args.no_git:
         return
+    subprocess.call(['git', 'pull', '--rebase', '--autostash'])
     subprocess.call(['git', 'add', file])
     if file.is_relative_to('games'):
         subprocess.call(['git', 'commit', '-m', f'{user_config.username} generated new game'])
     else:
         subprocess.call(['git', 'commit', '-m', f'{user_config.username} played a game'])
 
-    subprocess.call(['git', 'pull', '--rebase', '--autostash'])
     subprocess.call(['git', 'push'])
