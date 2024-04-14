4f0be4a47055519507dc899bb0aaaf2640675867
conor <conormurphyx@gmail.com>
2024-04-13 22:51:56 -0700

    * fixed posix problem

--- src/gitUtils.py
+++ src/gitUtils.py
@@ -10,9 +10,9 @@ def save_to_git(args, user_config, file):
         return
     subprocess.call(['git', 'pull', '--rebase', '--autostash'])
     subprocess.call(['git', 'add', file])
-    if file.is_relative_to('games'):
-        subprocess.call(['git', 'commit', '-m', f'{user_config.username} generated new game'])
+    if args.generate:
+        subprocess.call(['git', 'commit', '-m', f'{user_config.username} generated new game!'])
     else:
-        subprocess.call(['git', 'commit', '-m', f'{user_config.username} played a game'])
+        subprocess.call(['git', 'commit', '-m', f'{user_config.username} played a game!'])
 
     subprocess.call(['git', 'push'])
